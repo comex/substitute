44e224e908860425a7020089f3b0b93903bc2af7
comex <comexk@gmail.com>
2015-01-25 21:58:25 -0500

    forgot to commit this, predictably

---
+++ script/gen-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+debroot=out/debroot
+version="$(git describe --always --dirty)"
+rm -rf $debroot
+mkdir -p $debroot
+mkdir -p $debroot/usr/share/doc/substitute
+cp doc/installed-README.txt $debroot/usr/share/doc/README.txt
+cp substrate/lgpl-3.0.tar.xz $debroot/usr/share/doc/
+mkdir -p $debroot/usr/lib
+cp out/libsubstitute.dylib $debroot/usr/lib/libsubstitute.0.dylib
+ln -s libsubstitute.0.dylib $debroot/usr/lib/libsubstitute.dylib
+mkdir -p $debroot/usr/include/substitute
+cp lib/substitute.h $debroot/usr/include/substitute/
+cp substrate/substrate.h $debroot/usr/include/substitute/
+cp -a DEBIAN $debroot/
+sed "s#{VERSION}#$version#g" DEBIAN/control > $debroot/DEBIAN/control
+#... add bootstrap stuff
+# yay, old forks and deprecated compression
+dpkg-deb -Zlzma -b $debroot out/com.ex.substitute-$version.deb
+
+
+
+
