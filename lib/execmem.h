ea53601caa924e0b3404b2f195f6541d7fdc50ba
comex <comexk@gmail.com>
2016-04-19 22:04:59 -0400

    comment

--- lib/execmem.h
+++ lib/execmem.h
@@ -5,7 +5,8 @@ int execmem_alloc_unsealed(uintptr_t hint, void **page_p, size_t *size_p);
 int execmem_seal(void *page);
 void execmem_free(void *page);
 
-/* Write to foreign pages which are already RX or have unknown permissions.
+/* Write to "foreign" (i.e. owned by another library, not out-of-process) pages
+ * which are already RX or have unknown permissions.
  * If callback is not NULL, run it on all other threads 'atomically', in the
  * sense that it will be called on any thread which executed any of the old
  * instructions in the write region.
